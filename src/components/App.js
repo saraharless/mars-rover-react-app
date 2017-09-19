9316328e221994e1fbbf09f51a68c732bcc0b7ac
saraharless <sara.e.harless@gmail.com>
2017-09-19 14:10:35 -0500

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import apiKey from './apiKey';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>ghost toast</p>
+      </div>
+    );
+  }
+}
+
+export default App;
