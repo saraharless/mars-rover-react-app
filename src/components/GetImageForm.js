207f145fe21b073024ee89f62dec11053fd3fef0
saraharless <sara.e.harless@gmail.com>
2017-09-19 14:31:51 -0500

    add imports to all component files

---
+++ src/components/GetImageForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import apiKey from './components/apiKey.js';
+import GetImageButton from './components/GetImageButton.js';
+import ImageDisplay from './components/ImageDisplay.js';
+
+
+class GetImageForm extends Components {
+  constructor(props){
+    super(props);
+//bind
+    this.handlePhoto = this.handlePhoto.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+//set state
+    this.state = {
+
+    };
+  }
+  //handle photo setstate
+}
+// handle submit setstate
